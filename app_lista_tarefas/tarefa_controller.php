82b61accb1f4fcaa732bd55d6955f22ea91851e3
benetesla <78994881+benetesla@users.noreply.github.com>
2022-06-03 14:12:42 -0300

    Lista_Tarefas_PHP

---
+++ app_lista_tarefas/tarefa_controller.php
@@ -0,0 +1,77 @@
+<?php
+#cara aqui estav esquecendo de espeficificar o caminho do arquivo que está sendo incluido,foi bem louco porque não consegui resolver o problema tive que procurar na internet obrigado stackoverflow
+
+	require "../../app_lista_tarefas/tarefa.model.php";
+	require "../../app_lista_tarefas/tarefa.service.php";
+	require "../../app_lista_tarefas/conexao.php";
+
+
+	$acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
+
+	
+
+	if($acao == 'inserir' ) {
+		$tarefa = new Tarefa();
+		$tarefa->__set('tarefa', $_POST['tarefa']);
+
+		$conexao = new Conexao();
+
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefaService->inserir();
+
+		header('Location:nova_tarefa.php?inclusao=1');
+	
+	} else if($acao == 'recuperar') {
+		
+		$tarefa = new Tarefa();
+		$conexao = new Conexao();
+
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefas = $tarefaService->recuperar();
+	
+	} else if($acao == 'atualizar') {
+
+		$tarefa = new Tarefa();
+		$tarefa->__set('id', $_POST['id'])->__set('tarefa', $_POST['tarefa']);
+
+		$conexao = new Conexao();
+
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		if($tarefaService->atualizar()) {
+			header('location: todas_tarefas.php');
+		}
+
+
+	}else if ($acao == 'remover') {
+		$tarefa = new Tarefa();
+		$tarefa->__set('id', $_GET['id']);
+
+		$conexao = new Conexao();
+
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefaService->remover();
+
+		header('location: todas_tarefas.php');
+
+	} else if ($acao == 'concluir') {
+		$tarefa = new Tarefa();
+		$tarefa->__set('id', $_GET['id'])->__set('id_status', 2);
+
+		$conexao = new Conexao();
+
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefaService->Concluida();
+
+		header('location: todas_tarefas.php');
+
+	} else if ($acao == 'recuperarTarefas'){
+		$tarefa = new Tarefa();
+		$tarefa->__set('id_status', 1);
+
+		$conexao = new Conexao();
+
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefas = $tarefaService->recuperarTarefas();
+	}
+
+?>
